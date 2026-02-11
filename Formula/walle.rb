749051281cb4ada29d0fb3d85893f34d50af2660
kallepronk <kalle.pronk@harborn.com>
2026-02-11 08:44:48 +0100

    init

---
+++ Formula/walle.rb
@@ -0,0 +1,23 @@
+class Walle < Formula
+  desc "a trash compactor for your code comments"
+  homepage "https://github.com/kallepronk/wall-e"
+  url "https://github.com/kallepronk/wall-e/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "1e42b3178e0d06da3e26b5f74566f0a23884a4dfb2b845ed12022d4c21279cb5"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(output: bin/"walle"), "./..."
+
+    # 2. Create the second entrypoint (Symlink)
+    # This tells Homebrew: "Create a symlink named 'wall-e' that points to 'walle'"
+    bin.install_symlink "walle" => "wall-e"
+  end
+
+  test do
+    # specific tests to ensure both entrypoints work
+    system "#{bin}/walle", "--version"
+    system "#{bin}/wall-e", "--version"
+  end
+end
