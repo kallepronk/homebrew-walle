d1fc596a81970db627517400dc1854b489eedadb
kallepronk <kalle.pronk@harborn.com>
2026-03-04 14:16:20 +0100

    update v0.5.1

--- Formula/walle.rb
+++ Formula/walle.rb
@@ -1,8 +1,8 @@
 class Walle < Formula
   desc "a trash compactor for your code comments"
   homepage "https://github.com/kallepronk/wall-e"
-  url "https://github.com/kallepronk/wall-e/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "a7e31048c22b424d92f2c781fbba1da8f706b81c2f3b0117002d969fc2b9917d"
+  url "https://github.com/kallepronk/wall-e/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "99f2fdf5bb12b3af01abb411a92de500c9d4852d44b2624025dbaac7daa04eb9"
   license "MIT"
 
   depends_on "go" => :build
